5d13a5d3fa8c455fe50da2f7a692f857820294df
Matheus Ashton Silva <matheusashton@gmail.com>
2021-03-20 13:36:22 -0300

    [emacs] fixing font and ligatures with fira (requires emacs@28) and fixing git ignored files in treemacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 16))
+(setq doom-font (font-spec :family "Fira Code" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -68,7 +68,7 @@
  (add-to-list 'dtrt-indent-hook-mapping-list '(typescript-mode javascript typescript-indent-level)))
 
 ;; Ignoring .gitignore files / dirs
-(setq treemacs-python-executable "/usr/local/bin/python3")
+(setq treemacs-python-executable "/usr/bin/python3")
 (after! treemacs
   (treemacs-git-mode 'extended)
   (add-to-list 'treemacs-pre-file-insert-predicates #'treemacs-is-file-git-ignored?))
