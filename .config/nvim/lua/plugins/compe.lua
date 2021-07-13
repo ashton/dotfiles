210204e89a8e7792126e860ae3a22a80292134d9
Matheus Ashton Silva <matheus.ashton@nubank.com.br>
2021-07-13 15:57:58 -0300

    [neovim] compe tresholds

--- .config/nvim/lua/plugins/compe.lua
+++ .config/nvim/lua/plugins/compe.lua
@@ -8,12 +8,21 @@ compe.setup {
   debug = false,
   min_length = 1,
   preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+
 
   source = {
-    path = true;
-    buffer = true;
-    nvim_lsp = true;
-    nvim_lua = true;
+    path = true,
+    buffer = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    calc = true,
+    vim = true
     -- vsnip = true;
     -- ultisnips = true;
     -- luasnip = true;
