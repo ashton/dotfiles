e90fa4d8ed05fb5250acdea8d106266156d626c2
Matheus Ashton Silva <matheus.ashton@nubank.com.br>
2021-07-16 11:19:26 -0300

    [nvim] fixing nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,4 @@
 vim.g.nvim_tree_ignore = {'.pyc', 'node_modules', 'build', 'dist', '.git', 'tags', 'target', '.bs.js'}
-vim.g.nvim_tree_gitignore = true
-vim.g.nvim_tree_autoclose = true
-vim.g.nvim_tree_quit_on_open = true
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_autoclose = 1
+vim.g.nvim_tree_quit_on_open = 1
