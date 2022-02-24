7a8dd6d3e6d42e8b628615ef991ec50309a5a39b
Matheus Ashton Silva <matheus.ashton@nubank.com.br>
2022-02-23 21:42:31 -0300

    [nvim] updating cmp autopairs config

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,6 @@
-require "nvim-autopairs".setup()
-require "nvim-autopairs.completion.cmp".setup {
-  map_cr = true,
-  map_complete = true,
-  auto_select = false
-}
+require "nvim-autopairs".setup {}
+
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({map_char = {tex = ""}}))
