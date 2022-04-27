b8af1636a3bd73977f30160e64b3a054e7719cb6
Matheus Ashton Silva <matheus.ashton@nubank.com.br>
2022-04-26 23:27:06 -0300

    [nvim] fixing lsp bindings

--- .config/nvim/lua/mappings/plugins/lsp-saga.lua
+++ .config/nvim/lua/mappings/plugins/lsp-saga.lua
@@ -1,31 +1,31 @@
 local leader_mappings = {
   c = {
     name = "+code",
-    a = {"<cmd>Lspsaga code_action<CR>", "code action"},
-    d = {"<cmd>lua vim.lsp.buf.definition()<CR>", "go to definition"},
-    D = {"<cmd>LspTrouble lsp_references", "find references"},
-    h = {"<cmd>Lspsaga hover_doc<CR>", "documentation"},
-    p = {"<cmd>Lspsaga preview_definition<CR>", "preview definition"},
-    r = {"<cmd>Lspsaga rename<CR>", "rename symbol"},
-    x = {"<cmd>Lspsaga show_line_diagnostics<cr>", "line diagnostics"},
-    X = {"<cmd>LspTrouble lsp_workspace_diagnostics<CR>", "workspace diagnostics"}
+    a = {"<Cmd>Lspsaga code_action<CR>", "code action"},
+    d = {"<Cmd>lua vim.lsp.buf.definition()<CR>", "go to definition"},
+    D = {"<Cmd>Trouble lsp_references<CR>", "find references"},
+    h = {"<Cmd>Lspsaga hover_doc<CR>", "documentation"},
+    p = {"<Cmd>Lspsaga preview_definition<CR>", "preview definition"},
+    r = {"<Cmd>Lspsaga rename<CR>", "rename symbol"},
+    x = {"<Cmd>Lspsaga show_line_diagnostics<cr>", "line diagnostics"},
+    X = {"<Cmd>Trouble document_diagnostics<CR>", "workspace diagnostics"}
   }
 }
 
 local goto_mappings = {
   name = "+goto",
   d = {"<Cmd>lua vim.lsp.buf.definition()<CR>", "go to definition"},
-  D = {"<Cmd>LspTrouble lsp_references", "find references"}
+  D = {"<Cmd>Trouble lsp_references<CR>", "find references"}
 }
 
 local next_mappings = {
   name = "+next",
-  e = {"<cmd>Lspsaga diagnostic_jump_next<cr>", "next diagnostic"}
+  e = {"<Cmd>Lspsaga diagnostic_jump_next<cr>", "next diagnostic"}
 }
 
 local prev_mappings = {
   name = "+previous",
-  e = {"<cmd>Lspsaga diagnostic_jump_prev<cr>", "previous diagnostic"}
+  e = {"<Cmd>Lspsaga diagnostic_jump_prev<cr>", "previous diagnostic"}
 }
 
 vim.api.nvim_set_keymap("v", "<leader>ca", ":<C-U>Lspsaga range_code_action<CR>", {silent = true, noremap = true})
