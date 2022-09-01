fafc9cdad9fa91c19daba13cf684827a6cf5c2a3
Matheus Ashton Silva <matheus.ashton@nubank.com.br>
2022-09-01 12:22:52 -0300

    adding missing plugin config

---
+++ .config/nvim/lua/plugins/goto-preview.lua
@@ -0,0 +1,9 @@
+return {
+    setup = function()
+        print("ALOOOOOOOOOOOOW**********************")
+        require("goto-preview").setup {
+          dismiss_on_move = true
+        }
+    end
+}
+
