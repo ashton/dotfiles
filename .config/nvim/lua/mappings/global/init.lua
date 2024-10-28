c4264a602d4fbdb38c42bb5e43732e831ea23b27
Matheus Ashton Silva <matheusashton@gmail.com>
2024-10-28 20:30:40 -0300

    [nvim] organizing global bindings

--- .config/nvim/lua/mappings/global/init.lua
+++ .config/nvim/lua/mappings/global/init.lua
@@ -1,6 +1,8 @@
 local buffer_mappings = require "mappings.global.buffers"
 local window_mappings = require "mappings.global.windows"
 
-
-local global_mappings = {["<Tab>"] = {"<Plug>(matchup-%)", "Go to the matching pair"}}
+local global_mappings = {
+  ["<Tab>"] = { "<Plug>(matchup-%)", "Go to the matching pair" },
+  ["<Esc><Esc>"] = { "<Cmd>Noice dismiss<CR>", "Clear notifications" }
+}
 return vim.tbl_deep_extend("force", global_mappings, buffer_mappings, window_mappings)
