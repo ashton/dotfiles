bcfdcc370567d825f347de4d52958b74ef87f550
Matheus Ashton Silva <matheusashton@gmail.com>
2024-10-28 20:28:50 -0300

    [nvim] fixing statusline

--- .config/nvim/lua/statusline/components/lsp.lua
+++ .config/nvim/lua/statusline/components/lsp.lua
@@ -9,19 +9,19 @@ function lsp_server_component:init(options)
 end
 
 function lsp_server_component:update_status()
-    local msg = '--'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
+  local msg = '--'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
     return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
 end
 
 function lsp_server_component:apply_icon()
